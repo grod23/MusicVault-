62ba162da5f44b5b2bce7c2dc510af95444bf1fa
WuBstuff <wubstuff@csu.fullerton.edu>
2026-09-14 16:36:35 -0700

    React Frontpage

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="login-card">
+
+        <div className="logo">♫</div>
+
+        <h1>Music Vault</h1>
+        <p className="subtitle">Music for You</p>
+{/* Email input */}
+        <div className="input-box">
+          <span>✉</span>
+          <input
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+{/* Password Input */}
+        <div className="input-box">
+          <span>🔒</span>
+          <input
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+{/* Remember Login */}
+        <div className="options">
+          <label>
+            <input type="checkbox" />
+            Remember me
+          </label>
+{/* Forgot Password */}
+          <button className="forgot-button">
+            Forgot password?
+          </button>
+        </div>
+
+        <button className="login-button">
+          Login
+        </button>
+
+        <button className="register-button">
+          Register
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
